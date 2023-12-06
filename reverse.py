a46b660102c9da4277394b6309d8d4ae5ea465d9
MD NOMAN ISLAM <113965562+nomanislam222@users.noreply.github.com>
2023-12-06 16:29:00 +0600

    Create reverse.py
    Write a python program that reads 5 numbers from the user into a list, and then prints them in the reverse order.
    Hint: You may create a list to store the input numbers and then use loop to print them in reverse order

---
+++ reverse.py
@@ -0,0 +1,8 @@
+#task3
+b=[]
+for i in range(5):
+    a=int(input("Enter a number:"))
+    b.append(a)
+print("Printing values from the list in reverse order:")
+if i in range(len(b)-1,-1,-1):
+    print(b[i]
